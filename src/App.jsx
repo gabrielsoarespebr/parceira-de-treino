4b19c8a2da0e2d2a687b99d545fd1d02e48237a8
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-05-08 11:19:01 -0300

    feat: Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Homepage } from "./pages/Homepage/Homepage";
 
 function App() {
   return (
-    <>
-      <Homepage></Homepage>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/inicio" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
