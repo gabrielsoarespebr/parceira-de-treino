feb26dad18eeb8694bd38a9050d8688e03aa397a
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-05-14 11:37:14 -0300

    feat: Informações sobre usuárias, objetivos e esportes (utilizando enum e interface)

---
+++ src/data/interfaces/Usuaria.ts
@@ -0,0 +1,16 @@
+export interface Usuaria {
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  email: string;
+  instagramUrl: string;
+  dataNascimento: string;
+  endereco: string;
+  fotoPerfilUrl: string;
+  fotoVerificacaoUrl: string;
+  objetivo: number;
+  esportes: number[];
+  treinoTurno: number;
+  treinoHorario: string;
+  senha: string;
+}
