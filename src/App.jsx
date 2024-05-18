d27e77d3a9bf1cc7296e1962b3fa39e64eece6c3
Sabrina Rocha <sabrinaxavierdarocha@gmail.com>
2024-05-18 11:56:43 -0300

    feat: Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Homepage } from "./pages/Homepage/Homepage";
 import { Chat } from "./pages/Chat/Chat";
+import PaginaLogin from "./pages/PaginaLogin/PaginaLogin";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/home" element={<Homepage />} />
         <Route path="/inicio" element={<Homepage />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<PaginaLogin/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/PaginaLogin/PaginaLogin.jsx
@@ -0,0 +1,50 @@
+
+import { useState } from "react";
+import "./PaginaLogin.css";
+
+function PaginaLogin(){
+  const[email, setEmail] = useState("")
+  const[Password, setPassword] = useState("")
+    return (
+      <div className="container">
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form">
+
+              <span className="login-form-title">Bem vinda!</span>
+
+              <div className="wrap-input">
+                <input className={email !== "" ? 'has-val input' : 'input'}
+                 type="Email"
+                 value={email}
+                 onChange={e => setEmail(e.target.value) } />
+                <span className="focus-input" data-placeholder="Email">Email</span>
+              </div>
+
+              <div className="wrap-input">
+                <input className="input" type="Password" 
+                value={Password}
+                onChange={e => setPassword(e.target.value) } />
+                <span className="focus-input" data-placeholder="Password">Senha</span>
+              </div>
+
+              <a href="" className="esqueci">Esqueceu a senha?</a>
+
+              <div className="container-login-form-botao">
+                <button className="login-form-botao">Entrar</button>
+              </div>
+
+              <div className="criarconta">
+                <span className="criarconta1">Não possui conta?</span>
+                <a href="" className="criarconta2">Se cadastre</a>
+              </div>
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+
+  export default PaginaLogin
