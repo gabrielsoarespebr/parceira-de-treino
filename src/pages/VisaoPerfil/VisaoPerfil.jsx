a943bd0263724a2a548a1e50eb34a62db7a6fb4c
BatistaIsabella <isabella.batist30@gmail.com>
2024-05-27 15:07:23 -0300

    criação visualisar perfil

--- src/pages/VisaoPerfil/VisaoPerfil.jsx
+++ src/pages/VisaoPerfil/VisaoPerfil.jsx
@@ -6,6 +6,8 @@ import iconeInstagram from "../../assets/icons/icons8-instagram-50.png";
 import iconeWhatsapp from "../../assets/icons/icons8-whatsapp-50.png";
 import iconeTwitter from "../../assets/icons/icons8-twitterx-50.png";
 import parceiroBrenda from "../../assets/image/usuarias/parceira1Brenda.png";
+import iconeCoracao from "../../assets/icons/icons8-coração-48.png"
+
 
 export function VisaoPerfil() {
     
@@ -13,40 +15,46 @@ export function VisaoPerfil() {
     console.log('Path to Whatsapp icon:', iconeWhatsapp);
     console.log('Path to Twitter icon:', iconeTwitter);
     console.log('Path to Parceiro Brenda:', parceiroBrenda);
+    console.log('Pach to coracao icon', iconeCoracao );
    
 
     return (
         <>
-            <header>
-                <nav>
-                    <ul className="barraDeNavegacao">
-                        <li>
-                            <a href="">
-                                <i className="fa-solid fa-circle-user"></i>
-                                <p>Meu perfil</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fa-solid fa-comments"></i>
-                                <p>Chat</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/muraldeprofissionais">
-                                <span className="material-icons">sports</span>
-                                <p>Profissionais</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fa-solid fa-right-to-bracket"></i>
-                                <p>Sair</p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+           <header>
+        <img
+          src="./src/assets/image/logoBranco.png"
+          alt="Logotipo FitSister"
+          id="logoHeader"
+        />
+        <nav>
+          <ul className="barraDeNavegacao">
+            <li>
+              <a href="">
+                <i className="fa-solid fa-circle-user"></i>
+                <p>Meu perfil</p>
+              </a>
+            </li>
+            <li>
+              <a href="/chat">
+                <i className="fa-solid fa-comments"></i>
+                <p>Chat</p>
+              </a>
+            </li>
+            <li>
+              <a href="/muraldeprofissionais">
+                <span className="material-icons">sports</span>
+                <p>Profissionais</p>
+              </a>
+            </li>
+            <li>
+              <a href="/home">
+                <i className="fa-solid fa-right-to-bracket"></i>
+                <p>Sair</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
             <div className="CaixaVisuPerfil">
                 <div className="imagemNome">
@@ -55,6 +63,11 @@ export function VisaoPerfil() {
                     <h3 className="Localizacao">12km • Recife, PE</h3>
                     <h4 className="disponivel">Disponível...</h4>
                 </div>
+
+                    <div className='match'>
+                    <img className='coracao' src= {iconeCoracao} alt="logoCoracao" />
+                    </div>
+
                 <p className="descricao">
                     Gosto de correr em parques e também gosto bastante de me exercitar em academias de musculação.
                 </p>
