89d90319694467b309fcc92916344feeea5d1126
BatistaIsabella <isabella.batist30@gmail.com>
2024-05-28 23:29:26 -0300

    editar perfil

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import { TermosDeUso } from "./pages/TermosDeUso/TermosDeUso";
 import { PoliticaDePrivacidade } from "./pages/PoliticaDePrivacidade/PoliticaDePrivacidade";
 import PaginaLogin from "./pages/PaginaLogin/PaginaLogin";
 import RedefinirSenha from "./pages/RedefinirSenha/RedefinirSenha";
+import VisaoPerfil from "./pages/VisaoPerfil/VisaoPerfil";
+import EditarPerfil from "./pages/EditarPerfil/EditarPerfil";
 
 function App() {
   return (
@@ -29,6 +31,9 @@ function App() {
         <Route path="/termosdeuso" element={<TermosDeUso />} />
         <Route path="/politicadeprivacidade" element={<PoliticaDePrivacidade />} />
         <Route path="/redefinirsenha" element={<RedefinirSenha/>} /> 
+        <Route path="/visaoperfil" element={<VisaoPerfil/>} /> 
+        <Route path="/editarperfil" element={<EditarPerfil/>} /> 
+
       </Routes>
     </BrowserRouter>
   );
